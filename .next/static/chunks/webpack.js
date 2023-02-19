c440664756b617f8d3c5d09ed04ed8f1a2cc9999
Caroline Van <cmvan@stanford.edu>
2023-02-19 09:59:15 -0800

    fixed wrong answer gif

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -183,7 +183,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "dbf01333f9788cf4"; }
+/******/ 		__webpack_require__.h = function() { return "2c8acea2863215aa"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- pages/wrongans.js
+++ pages/wrongans.js
@@ -1,8 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import styles from '../styles/Wrong.module.css';
 
-import chat from './chatbox.png';
-
 export default function Incorrect() {
   const [currentFrame, setCurrentFrame] = useState(0);
 
