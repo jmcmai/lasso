45f85388d876fdb19cb3c0e7f4302973b3715b7c
Jenny Mai <jmcmai828@gmail.com>
2023-02-19 02:17:54 -0800

    updated index to store user

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,25 +1,36 @@
 import React from 'react'
 import { FormEvent, useEffect, useState } from 'react'
 import { useMutation, useQuery } from '../convex/_generated/react'
+import { Id } from "../convex/_generated/dataModel";
 import Badge from '../components/Badge'
 
 export default function App() {
+  // test user
+  const [userId, setUserId] = useState<Id<"users"> | null>(null);
+  const storeUser = useMutation("storeUser");
+  useEffect(() => {
+    // Store the user in the database.
+    // storeUser` gets the user information via the `auth` object on the server
+    // so we don't need to pass anything manually here.
+    async function createUser() {
+      const id = await storeUser();
+      setUserId(id);
+    }
+    createUser();
+    return () => setUserId(null);
+  }, [storeUser]);
+
   const notes = useQuery('getNotes') || []
 
   const [newNoteText, setNewNoteText] = useState('')
   const [newNoteTitle, setNewNoteTitle] = useState('')
   const addNote = useMutation('addNote')
 
-  const [name, setName] = useState('user')
-
-  useEffect(() => {
-    setName('User ' + Math.floor(Math.random() * 10000))
-  }, [])
-
   async function handleAddNote(event: FormEvent) {
     event.preventDefault()
+    setNewNoteTitle('')
     setNewNoteText('')
-    await addNote(newNoteText, name)
+    await addNote(newNoteTitle, newNoteText)
   }
   return (
     <main>
@@ -29,7 +40,7 @@ export default function App() {
         {notes.map((note:any) => (
           <li key={note._id.toString()}>
             <span>{note.title}:</span>
-            <span>{note.body}</span>
+            <span>{note.noteContent}</span>
             <span>{new Date(note._creationTime).toLocaleTimeString()}</span>
           </li>
         ))}
